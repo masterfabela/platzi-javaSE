33046d5c519c7376f63bef853ed9f5bc1b55b0e3
Jose Ramón Vieites <fabelaretorcida@gmail.com>
2020-07-10 23:28:36 +0200

    Clase 2

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        // Declarando una variable
+        int speed;
+        // Asignamos valor a variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Jose Ramon";
+        System.out.println(employeeName);
+    }
+
+}
