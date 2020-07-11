4756be14b80b1f89421e6b0141935242ad7537cd
Jose Ramón Vieites <fabelaretorcida@gmail.com>
2020-07-11 17:52:18 +0200

    Clase 6

---
+++ DataTypesJava/src/DataTypes.java
@@ -0,0 +1,9 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int n = 1234567891;
+        long nL = 12345678901L;
+
+        double nD= 123.456;
+        float nF = 123.456F;
+    }
+}
