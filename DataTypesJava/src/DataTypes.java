65280c4d1f20d041785d4e0807001258f84754e9
Jose Ramón Vieites <fabelaretorcida@gmail.com>
2020-07-11 18:11:40 +0200

    Clase 7

--- DataTypesJava/src/DataTypes.java
+++ DataTypesJava/src/DataTypes.java
@@ -5,5 +5,16 @@ public class DataTypes {
 
         double nD= 123.456;
         float nF = 123.456F;
+
+        var salary = 1000; // int
+        // pension 3%
+        var pension = salary*0.03; // double
+        var totalSalary = salary - pension;
+        System.out.println(salary);
+        System.out.println(pension);
+        System.out.println(totalSalary);
+
+        var employeeName = "Jose Ramon Vieites";
+        System.out.println("EMPLOYEE: "+employeeName+" SALARY: "+totalSalary);
     }
 }
